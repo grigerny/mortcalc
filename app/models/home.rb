c50657a491fc5ac53a0b3a9539cdb9c0e2d2a347
Gary Riger <grigerny@aol.com>
2014-07-27 16:59:28 -0400

    added validations

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,4 +1,6 @@
 class Home < ActiveRecord::Base   
   attr_accessible :asking_price, :seller_con, :contract_price, :name, :rate, :re_taxes, :ho_insurance
   
+  validates :asking_price, :rate, :name, :presence => true
+  
 end
