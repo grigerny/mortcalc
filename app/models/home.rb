30607c02affbfd7acd4ec851a62a98980efd4e10
Gary Riger <grigerny@aol.com>
2014-08-28 14:55:53 -0400

    updated validation

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,7 +1,7 @@
 class Home < ActiveRecord::Base   
   attr_accessible :asking_price, :seller_con, :contract_price, :name, :rate, :re_taxes, :ho_insurance
   
-  validates :asking_price, :rate, :name, :presence => true
+  validates :asking_price, :name, :presence => true
   
   def price(asking_price)
     self.asking_price.gsub(/\D/, '')
